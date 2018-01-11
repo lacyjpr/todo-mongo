934e0cbee7f174308f064b1f2f7527d0548132cf
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-01-11 10:05:32 -0800

    Add post todo route

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -6,7 +6,23 @@ const Todo = mongoose.model('todos');
 module.exports = app => {
   app.get('/api/todos', requireLogin, async (req, res) => {
     const todos = await Todo.find({ _user: req.user.id });
+    res.send(todos);
   });
 
-  res.send(todos);
+  app.post('/api/todos', requireLogin, async (req, res) => {
+    const { item, completed, completedAt, edited, editedAt } = req.body;
+
+    const todo = new Todo({
+      _user: req.user.id,
+      item,
+      createdAt: Date.now(),
+      completed,
+      completedAt,
+      edited,
+      editedAt,
+    });
+
+    await todo.save();
+    res.redirect('/todos');
+  });
 };
