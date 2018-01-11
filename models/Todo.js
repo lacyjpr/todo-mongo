9117d220623fc9f2345a7ab4e1515249f8536fbb
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-01-11 09:26:46 -0800

    Add createdAt to Todo model

--- models/Todo.js
+++ models/Todo.js
@@ -4,6 +4,7 @@ const { Schema } = require('schema');
 const todoSchema = new Schema({
   _user: { type: Schema.Types.ObjectIs, ref: 'User' },
   todo: String,
+  createdAt: Date,
   completed: Boolean,
   completedAt: Date,
   edited: Boolean,
