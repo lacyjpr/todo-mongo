b380dcf8886e21db24b8dd3d9975f90cc60b221c
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-01-14 15:50:13 -0800

    Add put route to update todo

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -25,4 +25,27 @@ module.exports = app => {
     await todo.save();
     res.json({ todo });
   });
+
+  app.put('/api/todos', requireLogin, (req, res) => {
+    Todo.findById(req.params._id, (err, todo) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        todo._user = req.body._user || todo._user;
+        todo.item = req.body.item || todo.item;
+        todo.createdAt = req.body.createdAt || todo.createdAt;
+        todo.completed = req.body.completed || todo.completed;
+        todo.completedAt = req.body.completedAt || todo.completedAt;
+        todo.edited = req.body.edited || todo.edited;
+        todo.editedAt = req.body.editedAt || todo.editedAt;
+
+        todo.save((err, todo) => {
+          if (err) {
+            res.status(500).send(err);
+          }
+          res.json({ todo });
+        });
+      }
+    });
+  });
 };
