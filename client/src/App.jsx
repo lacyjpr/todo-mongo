1da52957dd40dcb12584a30a5d738ebd3241732b
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-01-19 09:40:13 -0800

    Remove Link from imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import { Switch, BrowserRouter, Route, Link } from 'react-router-dom';
+import { Switch, BrowserRouter, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import * as actions from './actions';
 
 import Home from './components/Home';
-import About from './components/About';
 import Header from './components/Header';
 import TodoApp from './components/TodoApp';
 
@@ -21,7 +20,6 @@ class App extends Component {
           <Header />
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
             <Route path="/todoapp" component={TodoApp} />
           </Switch>
         </main>
