bd4cbc1c210f6d57505e694f47095636c906a1b2
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-01-19 09:58:24 -0800

    Edit Home copy

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Home = () => (
   <div>
-    <h2>Todos</h2>
-    <p>Log in to track your todo list</p>
+    <h2>Todo List</h2>
+    <p>Log in to track your todo list!</p>
   </div>
 );
 
